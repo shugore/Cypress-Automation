9903f0e3510eacfd926b5b684b2c4ebc98d6d45a
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-02-13 23:53:00 +0530

    test case for web inputs

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,8 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
 
-    baseUrl:'https://parabank.parasoft.com/',
+    //baseUrl:'https://parabank.parasoft.com/',
+    baseUrl:'https://practice.expandtesting.com/#tools',
   
 
     setupNodeEvents(on, config) {
---
+++ cypress/pageobjects/webInputsPage.js
@@ -0,0 +1,36 @@
+export class webInputsPage {
+
+
+    weblocators = {
+
+        webInputs: '[href="/inputs"].my-link',
+        inputNumber: '#input-number.input-box',
+        inputText: '#input-text.input-box',
+        inputPassword: '#input-password.input-box',
+        inputDate: '#input-date.input-box',
+       
+    }
+
+    clickOnWebInputs() {
+
+        cy.get(this.weblocators.webInputs).should('be.visible').click()
+    }
+
+    enterInputNumber(inputNumber) {
+
+        cy.get(this.weblocators.inputNumber).should('be.visible').type(inputNumber)
+    }
+
+    enterInputText(inputText) {
+
+        cy.get(this.weblocators.inputText).should('be.visible').type(inputText)
+    }
+
+    enterInputPassword(inputPassword) {
+
+        cy.get(this.weblocators.inputPassword).should('be.visible').type(inputPassword)
+    }
+    
+   
+
+}
