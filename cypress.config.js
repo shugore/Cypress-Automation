53d2ada78510b5b084fe9a5528bbb6deec4b833c
SHUBHAM  GORE <shubhamgore001@gmail.com>
2025-02-15 00:44:08 +0530

    html reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,15 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+
+  reporter: 'cypress-mochawesome-reporter',
+
+  charts: true,
+
+  video: true,
+
+  embeddedScreenshots: true,
+
   e2e: {
 
     baseUrl:'https://parabank.parasoft.com/',
@@ -8,7 +17,7 @@ module.exports = defineConfig({
 
     setupNodeEvents(on, config) {
       // implement node event listeners here
-
+    require('cypress-mochawesome-reporter/plugin')(on);
      
     },
    },
